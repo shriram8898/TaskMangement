d4b0085b4b16289372f17e288f5811b71e70f610
shriram8898 <51223184+shriram8898@users.noreply.github.com>
2020-05-15 13:10:57 +0530

    Add files via upload

---
+++ Task Management/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Task_Management
+{
+    
+    public partial class Form1 : Form
+    {
+        public static string email = "";
+        public static string name = "";
+        MySqlConnection con;
+        MySqlCommand cmd;
+        MySqlDataReader dr;
+        
+        Task task = new Task();
+        public Form1()
+        {
+            InitializeComponent();
+            MaximizeBox = false;
+            con = new MySqlConnection("Server=localhost;Database=stark;user=root;Pwd=tonystark;SslMode=none");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string user = txtUser.Text;
+            string pass = txtPass.Text;
+            cmd = new MySqlCommand();
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "SELECT * FROM user where email='" + txtUser.Text + "' AND password='" + txtPass.Text + "'";
+            dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                email = dr.GetString("email");
+                name = dr.GetString("username");
+                //MessageBox.Show("Login sucess Welcome to Homepage https://csharp-console-examples.com");
+                task.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Login please check username and password");
+            }
+            con.Close();
+        }
+
+        private void btRegister_Click(object sender, EventArgs e)
+        {
+            Register register = new Register();
+            this.Hide();
+            register.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            email = "";
+            name = "";
+        }
+    }
+}
